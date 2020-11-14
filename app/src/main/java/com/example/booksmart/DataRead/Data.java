69fa7138fa83299f20141eb406ff2a7b589c6792
Hardik Kapadia <thecoderenroute@gmail.com>
2020-11-14 15:16:50 +0530

    Fixed File acces by converting them to Android resource

--- app/src/main/java/com/example/booksmart/DataRead/Data.java
+++ app/src/main/java/com/example/booksmart/DataRead/Data.java
@@ -2,21 +2,27 @@ package com.example.booksmart.DataRead;
 
 import android.util.Log;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.booksmart.Elements.Book;
 import com.example.booksmart.Elements.Categories;
 import com.example.booksmart.People.Person;
 import com.example.booksmart.People.User;
+import com.example.booksmart.R;
 import com.example.booksmart.entry.MainActivity;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Data {
+public class Data extends AppCompatActivity {
 
     private ArrayList<User> users;
     private ArrayList<Book> books;
@@ -43,15 +49,40 @@ public class Data {
         this.books = books;
     }
 
-    public void readUsers() {
+    /*public void readUsers() {
 
-        File uFile = new File("src/main/java/com/example/booksmart/Data/Users.csv");
+        File uFile = new File("src/main/java/com/example/booksmart/Data/users.csv");
 
         Log.i("User file", Boolean.toString(uFile.isFile()));
 
         ArrayList<User> users = new ArrayList<>();
 
-        try (Scanner readUsers = new Scanner(uFile)) {
+        InputStream isr = getResources().openRawResource(R.raw.users);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(isr));
+
+        String line;
+
+        try {
+            while (true) {
+                line = reader.readLine();
+                if (line == null) break;
+                String[] parts = line.split(",");
+
+                int id = Integer.parseInt(parts[0]);
+                String email = parts[1];
+                long mobile = Long.parseLong(parts[2]);
+                String pw = parts[3];
+
+                String name = parts[5];
+
+                users.add(new User(new Person(name), email, mobile, pw, id));
+            }
+        } catch (IOException e) {
+            Log.wtf("Reading Users", "Error");
+            e.printStackTrace();
+        }
+
+        *//*try (Scanner readUsers = new Scanner(uFile)) {
 
             while (readUsers.hasNextLine()) {
 
@@ -67,7 +98,7 @@ public class Data {
 
                 users.add(new User(new Person(name), email, mobile, pw, id));
 
-                /*if (parts.length == 6) {
+                *//**//*if (parts.length == 6) {
                     fName = parts[4];
                     lName = parts[5];
                     users.add(new User(new Person(fName, lName), email, mobile, pw, id));
@@ -76,18 +107,18 @@ public class Data {
                         name += parts[i];
                     }
                     users.add(new User(new Person(name), email, mobile, pw, id));
-                }*/
+                }*//**//*
 
             }
         } catch (FileNotFoundException fnfe) {
             Log.i("User's File", "Not found");
-        }
+        }*//*
 
         Log.i("Users in users", Integer.toString(users.size()));
 
         this.users = users;
 
-    }
+    }*/
 
     public void writeUsers() {
 
--- app/src/main/java/com/example/booksmart/entry/MainActivity.java
+++ app/src/main/java/com/example/booksmart/entry/MainActivity.java
@@ -12,8 +12,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.booksmart.DataRead.Data;
+import com.example.booksmart.Elements.Book;
+import com.example.booksmart.Elements.Categories;
+import com.example.booksmart.People.Person;
 import com.example.booksmart.People.User;
 import com.example.booksmart.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -66,11 +75,87 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if (data == null) {
             data = new Data();
-            data.readUsers();
-            data.readBooks();
+            this.readUsersFromRes();
+            this.readBooksFromRes();
             Log.i("data", "Initializing");
             Log.i("Size of Users", Integer.toString(data.getAllUsers().size()));
             Log.i("Size of Books", Integer.toString(data.getAllBooks().size()));
         }
+    }
+
+    public void readBooksFromRes() {
+
+        ArrayList<Book> books = new ArrayList<>();
+
+        InputStream isr = getResources().openRawResource(R.raw.books);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(isr));
+
+        String line;
+
+        Categories[] allCategories = Categories.values();
+
+        try {
+            while (true) {
+                line = reader.readLine();
+                if (line == null) break;
+
+                String[] parts = line.split(",");
+
+                if (parts.length == 6) {
+
+                    int bookId = Integer.parseInt(parts[0].trim());
+                    int userId = Integer.parseInt(parts[1].trim());
+
+                    int year = Integer.parseInt(parts[2].trim());
+                    String name = parts[3].replace("$", ",").trim();
+                    Person author = new Person(parts[4].trim());
+
+                    Categories category = allCategories[Integer.parseInt(parts[5])];
+
+                    books.add(new Book(name, author, year, bookId, userId, category));
+
+                } else {
+                    throw new IllegalArgumentException();
+                }
+            }
+        } catch (IOException e) {
+            Log.wtf("Reading Books", "error");
+            e.printStackTrace();
+        }
+
+        data.setBooks(books);
+
+    }
+
+    public void readUsersFromRes() {
+
+        ArrayList<User> users = new ArrayList<>();
+
+        InputStream isr = getResources().openRawResource(R.raw.users);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(isr));
+
+        String line;
+
+        try {
+            while (true) {
+                line = reader.readLine();
+                if (line == null) break;
+                String[] parts = line.split(",");
+
+                int id = Integer.parseInt(parts[0]);
+                String email = parts[1];
+                long mobile = Long.parseLong(parts[2]);
+                String pw = parts[3];
+
+                String name = parts[4];
+
+                users.add(new User(new Person(name), email, mobile, pw, id));
+            }
+        } catch (IOException e) {
+            Log.wtf("Reading Users", "Error");
+            e.printStackTrace();
+        }
+
+        data.setUsers(users);
     }
 }
